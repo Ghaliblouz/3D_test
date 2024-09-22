8331ca9fb2c92672ad948f31a299b8c56ae54532
Ghaliblouz <blouz.elghali@gmail.com>
2024-09-22 16:34:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+function App() {
+
+  return (
+  <>
+    
+    <Canvas camera={{position: [3,3,3]}}>
+    <color attach={'background'} args={['#729fe8']} />
+    <OrbitControls />
+      <mesh>
+        <boxGeometry args={[0.5,0.5,0.5]} />
+        <meshNormalMaterial />
+      </mesh>
+    </Canvas>
+  </>
+  )
+}
+
+export default App
